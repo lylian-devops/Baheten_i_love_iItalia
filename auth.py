3b19d5f82e7aff57a5287d455b0d741da03b55b4
doina <daminica@live.it>
2026-05-27 16:39:58 +0200

    return API

--- auth.py
+++ auth.py
@@ -12,6 +12,6 @@ async def verify_api_key(api_key: str = Security(api_key_header)):
     if not api_key or api_key != API_KEY:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="API Key non valida o mancante"
+            detail="API Key non valida o mancanta"
         )
     return api_key
