8adf01600c5a2af8bf7d57902a3ca0c777c7b66e
doina <daminica@live.it>
2026-05-27 16:02:05 +0200

    //error 404//

--- routes.py
+++ routes.py
@@ -89,6 +89,6 @@ def update_aeroporto(
         return updated
     except ValueError as e:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_404_BAD_REQUEST,
             detail=str(e)
         )
